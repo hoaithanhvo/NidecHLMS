b27875e5b7fa0280b9253a8d57736c8f2a74f4e6
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2026-04-18 06:45:49 +0700

    Design Database Phase 1 (ver9.1)

--- Domain/Entities/T_USER_TRAINING_ENROLLMENT.cs
+++ Domain/Entities/T_USER_TRAINING_ENROLLMENT.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NidecSystemShared.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class T_USER_TRAINING_ENROLLMENT
+    internal class T_USER_TRAINING_ENROLLMENT : BaseEntity<int>
     {
+
+        public int ParticipantId { get; set; }
+        public int TrainingContentId { get; set; }
+        public int CurrentStepId { get; set; }
+        public int CurrentFlowStepId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime? CompleteDate { get; set; }
+        public decimal ProgressPercent { get; set; }
+        public T_USER_TRAINING_PROGRESS T_UserTrainingProgress { get; set; }
+        public M_USER M_User { get; set; }
+        public M_TRAINING_CONTENT M_TrainingContent { get; set; }
+        public M_TRAINING_CONTENT_STEP M_TrainingContentStep { get; set; }
+        public M_TRAINING_CONTENT_FLOW_STEP M_TrainingContentFlowStep { get; set; }
     }
 }
--- Domain/Entities/T_USER_TRAINING_PROGRESS.cs
+++ Domain/Entities/T_USER_TRAINING_PROGRESS.cs
@@ -9,18 +9,14 @@ namespace Domain.Entities
 {
 	public class T_USER_TRAINING_PROGRESS : BaseEntity<int>
 	{
-		public int UserId {get;set;}
-		public int TrainingContentId {get;set;}
-		public int TrainingContentStepId { get;set;}
-		public int TrainingContentStepFlowId { get;set;}
-		public int StatusId { get;set;}
-		public DateTime CompleteDate { get;set;}
-		public bool? IsDeleted { get; set; }
-		public DateTime? DeleteDate { get; set; }
+        public int Id { get; set; }
+        public int ParticipantId { get; set; }
+        public int TrainingContentId { get; set; }
+        public int StepId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime ActionDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
 		public M_USER M_User { get;set;}
-		public M_TRAINING_CONTENT M_TrainingContent { get;set;}
-		public M_TRAINING_CONTENT_STEP M_TrainingContentStep { get;set;}
-		public M_TRAINING_CONTENT_FLOW_STEP M_TrainingContentFlowStep { get;set;}
 		public M_STATUS M_Status { get;set;}	
 	}
 }

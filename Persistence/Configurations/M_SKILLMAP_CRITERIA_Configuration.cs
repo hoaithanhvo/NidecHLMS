bb8845e52b71b7c7148adccc2b84a1c0a9346442
VOHOAITHANH <thanh.vohoai@nidec.com>
2026-04-21 08:13:20 +0700

    Complete merge

--- Persistence/Configurations/M_SKILLMAP_CRITERIA_Configuration.cs
+++ Persistence/Configurations/M_SKILLMAP_CRITERIA_Configuration.cs
@@ -4,17 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<<< HEAD:Persistence/Configurations/M_SKILLMAP_CRITERIA_Configuration.cs
+
 namespace Persistence.Configurations
 {
     public class M_SKILLMAP_CRITERIA_Configuration
     {
     }
-========
-namespace Infrastructure.Models
-{
-	internal class IDMResponse
-	{
-	}
->>>>>>>> main:Infrastructure/Models/IDMResponse.cs
 }
